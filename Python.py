a55e0c69850ee3147c3aa1bfb949ca499f9564b5
Md Abu Sayem Musa <143425981+abusayemmusa@users.noreply.github.com>
2026-07-08 19:33:19 +0600

    Implement basic calculator operations in Python

---
+++ Python.py
@@ -0,0 +1,16 @@
+cal = input("Enter the operation: add, sub, mul, div:").lower()
+user = int(input("Enter the first number: "))
+user2 = int(input("Enter the secend number: "))
+
+if cal == "add":
+  add = user + user2
+  print(add)
+if cal == "sub":
+  sub = user - user2
+  print(sub)
+if cal == "mul":
+  mul = user * user2
+  print(mul)
+if cal == "div":
+  div = user / user2
+  print(div)
