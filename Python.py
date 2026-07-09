3994b79f0d295b26f26c89909b9464d2e79d8195
Md Abu Sayem Musa <143425981+abusayemmusa@users.noreply.github.com>
2026-07-10 04:24:39 +0600

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,17 +1,46 @@
-cal = input("Enter the operation: add, sub, mul, div:").lower()
-user = int(input("Enter the first number: "))
-user2 = int(input("Enter the secend number: "))
+========================================================================================================
+# Project 1
+# Calculator
+========================================================================================================
+operator = input("Please enter addition, subtraction, multiplication, or division: ")
 
-if cal == "add":
-  add = user + user2
-  print(add)
-if cal == "sub":
-  sub = user - user2
-  print(sub)
-if cal == "mul":
-  mul = user * user2
-  print(mul)
-if cal == "div":
-  div = user / user2
-  print(div)
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+
+if operator == "addition":
+    result = num1 + num2
+    print(f"The result of addition is: {result:.2f}")
+elif operator == "subtraction":
+    result = num1 - num2
+    print(f"The result of subtraction is: {result}")
+elif operator == "multiplication":
+    if num2 != 0:
+        result = num1 * num2
+        print(f"The result of multiplication is: {result:.2f}")
+    else:
+        print("Error: Multiplication by zero is not allowed.")
+elif operator == "division":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result of division is: {result:.2f}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operator. Please enter addition, subtraction, multiplication, or division.")
+  
+========================================================================================================
+# Project 2
+# even-odd-checker
+========================================================================================================
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is even.")
+elif number % 2 != 0:
+    print(f"{number} is odd.") 
+else:
+    print(f"{number} is neither even nor odd.")
+========================================================================================================
+========================================================================================================
+
+
 
