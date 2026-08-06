e8da4dd12a00bde54505c50c4011d7383907aaf9
Md Abu Sayem Musa <143425981+abusayemmusa@users.noreply.github.com>
2026-08-06 23:20:46 +0600

    Update Python_project.py

--- Python_project.py
+++ Python_project.py
@@ -205,6 +205,20 @@ print(f"Your score is: {score}/{len(questions)} ({score_percentage}%)")
 # Project 9
 # bmi calculator math python
 ========================================================================================================
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print("You are underweight.")
+elif 18.5 <= bmi < 24.9:
+    print("You have a normal weight.")
+elif 25 <= bmi < 29.9:
+    print("You are overweight.")
+else:
+    print("You are obese.")
+========================================================================================================
 # Project 10
 # Word Counter String methods
 ========================================================================================================
